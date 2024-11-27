08aef74da2dfaf34eb7399c71dbf55df4f6d2ff6
katarn024 <52570486+katarn024@users.noreply.github.com>
2024-11-27 10:28:47 +0300

    4.1. Разбор первой программы

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Pasha");
+        System.out.println("07.02.1997");
+    }
+}
