dd37ac39d05e49bcf0b2e41d2045c720c7ec8e70
katarn024 <52570486+katarn024@users.noreply.github.com>
2024-12-26 16:31:11 +0300

    1.1.2. - 4. Методы

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,11 +1,15 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-    public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onelusTwo = one + two;
-        System.out.println(onelusTwo);
 
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        Calculator.plus(100, 500);
+        Calculator.plus(4, 2);
+        Calculator.plus(3, 5);
     }
 }
