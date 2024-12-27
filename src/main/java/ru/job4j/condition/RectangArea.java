e236a01cd88b8f34d4e62422549dd6d2c72f6e93
katarn024 <52570486+katarn024@users.noreply.github.com>
2024-12-27 14:59:48 +0300

    1.1.2. - 8. Стороны прямоугольника

---
+++ src/main/java/ru/job4j/condition/RectangArea.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class RectangArea {
+
+    public static double square(double p, double k) {
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double s = l * h;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        double result = RectangArea.square(4, 1);
+        System.out.println(result);
+        result = RectangArea.square(6, 2);
+        System.out.println(result);
+    }
+}
