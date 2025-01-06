47591abd8a380634c2809d901d2a75215f7c5373
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-01-06 17:05:40 +0300

    1.1.3. Тестирование - 1. Модульные тесты.

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -3,7 +3,6 @@ package ru.job4j.converter;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ConverterTest {
 
