b5ed5c952ca3ad82ea53a5f74f9354daa500cd26
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-01-09 15:08:00 +0300

    1.1.4. Оператор ветвления - 4.3.1. Сокращенный оператор &&

---
+++ src/main/java/ru/job4j/condition/And.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class And {
+    public static void main(String[] args) {
+        boolean first = true;
+        boolean second = false;
+        boolean join = first && second;
+        System.out.println(join);
+    }
+}
