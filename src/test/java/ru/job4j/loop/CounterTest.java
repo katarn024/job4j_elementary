d0b2f663e4f53effff9098ef339e6c4629bdd78a
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-01-09 22:41:14 +0300

    1.1.4. Оператор ветвления - 5.0.1. Сумма чисел

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CounterTest {
+
+    @Test
+    void whenStart10Finish0ThenSum0() {
+        int start = 10;
+        int finish = 0;
+        int expected = 0;
+        int output = Counter.sum(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart0Finish5ThenSum15() {
+        int start = 0;
+        int finish = 5;
+        int expected = 15;
+        int output = Counter.sum(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+}
