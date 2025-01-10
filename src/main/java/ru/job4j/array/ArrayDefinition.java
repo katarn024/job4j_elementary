1d54ba7a18bbf5800bfa97340700d0f11cfa5217
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-01-10 22:06:28 +0300

    1.1.6. Массивы - 6.0.5. Массивы и цикл for-each

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -15,10 +15,9 @@ public class ArrayDefinition {
         names[1] = "Маша";
         names[2] = "Саша";
         names[3] = "Галя";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+        for (String name : names) {
+            System.out.println(name);
+        }
 
     }
 }
