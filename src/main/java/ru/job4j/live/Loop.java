ff48a62790fb6d5adebf8445ee838cd6d673231b
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-01-10 10:00:33 +0300

    1.1.5. Циклы - 6.1. Генерация конструкций в IDEA

---
+++ src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,12 @@
+package ru.job4j.live;
+
+public class Loop {
+    /* сгенериравано с помощью psvm */
+    public static void main(String[] args) {
+        /* сгенериравано с помощью fori */
+        for (int i = 0; i <= 10; i++) {
+            /* сгенериравано с помощью sout */
+            System.out.println("i");
+        }
+    }
+}
