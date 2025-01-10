e8593ee614c78715f47f49f0d01b9307377b82ce
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-01-10 21:46:14 +0300

    1.1.6. Массивы - 6.0.3. Заполнение массива

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,16 @@ public class ArrayDefinition {
         System.out.println("Количество ячеек массива ages: " + ages.length);
         System.out.println("Количество ячеек массива surnames: " + surnames.length);
         System.out.println("Количество ячеек массива prices: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Оля";
+        names[1] = "Маша";
+        names[2] = "Саша";
+        names[3] = "Галя";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
